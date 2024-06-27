d12d6a6973aeac2183bc74ad980824f2852ca333
DURGA PRASAD SAHOO <122190960+durga6666@users.noreply.github.com>
2024-06-28 00:00:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,436 @@
+data =[
+
+    {
+        name : "Mercury",
+        color : "#909090",
+        shortDesc : "Mercury—the smallest planet in our solar system and closest to the Sun—is only slightly larger than Earth's Moon. Mercury is the fastest planet, zipping around the Sun every 88 Earth days.",
+
+        longDesc : "The smallest planet in our solar system and nearest to the Sun, Mercury is only slightly larger than Earth's Moon. From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as seven times brighter. Despite its proximity to the Sun, Mercury is not the hottest planet in our solar system - that title belongs to nearby Venus, thanks to its dense atmosphere. Because of Mercury's elliptical - egg-shaped - orbit, and sluggish rotation, the Sun appears to rise briefly, set, and rise again from some parts of the planet's surface. The same thing happens in reverse at sunset.s",
+
+        yearLength : [88, "Days"],
+        sunDistance : [29843396, 0.4],
+        moons : 0,
+        namesake : "Roman God of Speed",
+        lightTime : 2.670098,
+        type : "Terrestrial"
+    },
+    {
+        name : "Venus",
+        color : "#e9a1ac",
+        shortDesc : "Venus spins slowly in the opposite direction from most planets. A thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system.",
+
+        longDesc : "Venus is the second planet from the Sun and is Earth's closest planetary neighbor. It's one of the four inner, terrestrial (or rocky) planets, and it's often called Earth's twin because it's similar in size and density. These are not identical twins, however - there are radical differences between the two worlds. Venus has a thick, toxic atmosphere filled with carbon dioxide and it's perpetually shrouded in thick, yellowish clouds of sulfuric acid that trap heat, causing a runaway greenhouse effect. It's the hottest planet in our solar system, even though Mercury is closer to the Sun. Surface temperatures on Venus are about 900 degrees Fahrenheit (475 degrees Celsius) - hot enough to melt lead. The surface is a rusty color and it's peppered with intensely crunched mountains and thousands of large volcanoes. Scientists think it's possible some volcanoes are still active.",
+
+        yearLength : [225,"Days"],
+        sunDistance : [67153104, 0.7],
+        moons : 0,
+        namesake : "Roman Goddess of Love",
+        lightTime : 6.008183,
+        type : "Terrestrial"
+    },
+    {
+        name : "earth",
+        color : "#419FD6",
+        shortDesc : "Earth—our home planet—is the only place we know of so far that's inhabited by living things. It's also the only planet in our solar system with liquid water on the surface.",
+
+        longDesc : "Our home planet is the third planet from the Sun, and the only place we know of so far that's inhabited by living things. While Earth is only the fifth largest planet in the solar system, it is the only world in our solar system with liquid water on the surface. Just slightly larger than nearby Venus, Earth is the biggest of the four planets closest to the Sun, all of which are made of rock and metal. The name Earth is at least 1,000 years old. All of the planets, except for Earth, were named after Greek and Roman gods and goddesses. However, the name Earth is a Germanic word, which simply means “the ground.”",
+
+        yearLength : [365.25,"Days"],
+        sunDistance : [94475611, 1],
+        moons : 1,
+        namesake : "Variation of 'The Ground' in many languages",
+        lightTime : 8.452725,
+        type : "Terrestrial"
+    },
+    {
+        name : "Mars",
+        color : "#DF6136",
+        shortDesc : "Mars is a dusty, cold, desert world with a very thin atmosphere. There is strong evidence Mars was—billions of years ago—wetter and warmer, with a thicker atmosphere.",
+
+        longDesc : "Mars is the fourth planet from the Sun = a dusty, cold, desert world with a very thin atmosphere. Mars is also a dynamic planet with seasons, polar ice caps, canyons, extinct volcanoes, and evidence that it was even more active in the past. Mars is one of the most explored bodies in our solar system, and it's the only planet where we've sent rovers to roam the alien landscape.",
+
+        yearLength : [1.88,"Years"],
+        sunDistance : [129093739, 1.5],
+        moons : 2,
+        namesake : "Roman God of War",
+        lightTime : 8.452725,
+        type : "Terrestrial"
+    },
+    {
+        name : "Jupiter",
+        color : "#F5D7BD",
+        shortDesc : "Jupiter is more than twice as massive than the other planets of our solar system combined. The giant planet's Great Red spot is a centuries-old storm bigger than Earth.",
+
+        longDesc : "Jupiter has a long history of surprising scientists - all the way back to 1610 when Galileo Galilei found the first moons beyond Earth. That discovery changed the way we see the universe. Fifth in line from the Sun, Jupiter is, by far, the largest planet in the solar system - more than twice as massive as all the other planets combined. Jupiter's familiar stripes and swirls are actually cold, windy clouds of ammonia and water, floating in an atmosphere of hydrogen and helium. Jupiter's iconic Great Red Spot is a giant storm bigger than Earth that has raged for hundreds of years.",
+
+        yearLength : [11.86,"Years"],
+        sunDistance : [461103739, 5.2],
+        moons : 79,
+        namesake : "King of the Roman Gods",
+        lightTime : 41.254916,
+        type : "Gas Giant"
+    },
+    {
+        name : "Saturn",
+        color : "#F5DCB4",
+        shortDesc : "Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system. The other giant planets have rings, but none are as spectacular as Saturn's.",
+
+        longDesc : "Saturn is the sixth planet from the Sun and the second-largest planet in our solar system. Adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings - made of chunks of ice and rock - but none are as spectacular or as complicated as Saturn's. Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium.",
+
+        yearLength : [29.45,"Years"],
+        sunDistance : [917907449, 0.5],
+        moons : 62,
+        namesake : "Father of Jupiter",
+        lightTime : 82.125109,
+        type : "Gas Giant"
+    },
+    {
+        name : "Uranus",
+        color : "#A6D7DE",
+        shortDesc : "Uranus—seventh planet from the Sun—rotates at a nearly 90-degree angle from the plane of its orbit. This unique tilt makes Uranus appear to spin on its side.",
+
+        longDesc : "Uranus is the seventh planet from the Sun, and has the third-largest diameter in our solar system. It was the first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star. It was two years later that the object was universally accepted as a new planet, in part because of observations by astronomer Johann Elert Bode. Herschel tried unsuccessfully to name his discovery Georgium Sidus after King George III. Instead, the scientific community accepted Bode's suggestion to name it Uranus, the Greek god of the sky, as suggested by Bode.",
+
+        yearLength : [84,"Years"],
+        sunDistance : [1856902225, 19.8],
+        moons : 27,
+        namesake : "Greek God of the Sky",
+        lightTime : 163.787016,
+        type : "Ice Giant"
+    },
+    {
+        name : "Neptune",
+        color : "#395DBD",
+        shortDesc : "Neptune—the eighth and most distant major planet orbiting our Sun—is dark, cold and whipped by supersonic winds. It was the first planet located through mathematical calculations, rather than by telescope.",
+
+        longDesc : "Dark, cold, and whipped by supersonic winds, ice giant Neptune is the eighth and most distant planet in our solar system. More than 30 times as far from the Sun as Earth, Neptune is the only planet in our solar system not visible to the naked eye and the first predicted by mathematics before its discovery. In 2011 Neptune completed its first 165-year orbit since its discovery in 1846. NASA's Voyager 2 is the only spacecraft to have visited Neptune up close. It flew past in 1989 on its way out of the solar system.",
+
+        yearLength : [164.81,"Years"],
+        sunDistance : [2780873246, 30.1],
+        moons : 14,
+        namesake : "Roman God of the Sea",
+        lightTime : 248.804517,
+        type : "Ice Giant"
+    },
+    {
+        name : "Pluto",
+        color : "#A7B5B9",
+        shortDesc : "Pluto is a complex world of ice mountains and frozen plains. Once considered the ninth planet, Pluto is the largest member of the Kuiper Belt and the best known of a new class of worlds called dwarf planets.",
+
+        longDesc : "Pluto is a dwarf planet in the Kuiper Belt, a donut-shaped region of icy bodies beyond the orbit of Neptune. There may be millions of these icy objects, collectively referred to as Kuiper Belt objects (KBOs) or trans-Neptunian objects (TNOs), in this distant region of our solar system. Pluto - which is smaller than Earth's Moon - has a heart-shaped glacier that's the size of Texas and Oklahoma. This fascinating world has blue skies, spinning moons, mountains as high as the Rockies, and it snows - but the snow is red.",
+
+        yearLength : [248.89,"Years"],
+        sunDistance : [3213469421, 39],
+        moons : 5,
+        namesake : "Roman God of the Underworld",
+        lightTime : 287.508864,
+        type : "Ice Giant"
+    }
+]
+const transformData = [
+    {
+        transY : 100,
+        transX : 0,
+        scale : 2,
+        opacity : 0
+    },
+    {
+        transY : 0,
+        transX : 0,
+        scale : 1,
+        opacity : 1
+    },
+    {
+        transY : -50,
+        transX : 0,
+        scale : 0.2,
+        opacity : .5
+    },
+    {
+        transY : -60,
+        transX : 0,
+        scale : 0.075,
+        opacity : .1
+    },
+    {
+        transY : -63.5,
+        transX : 0,
+        scale : 0.035,
+        opacity : 0
+    },
+    {
+        transY : -25,
+        transX : 60,
+        scale : 1,
+        opacity : 1
+    },
+
+    {
+        transY : -60,
+        transX : 10,
+        scale : 0.075,
+        opacity : 0
+    },
+    {
+        transY : -25,
+        transX : -60,
+        scale : 1,
+        opacity : 1
+    },
+];
+
+const planetNamber = document.querySelector(".planet-namber");
+const planetName = document.querySelector(".planet-name");
+const infoBtn = document.querySelector(".info-btn");
+const closeInfoBtn = document.querySelector(".close-info");
+const planetDescriptions = document.querySelectorAll(".planet-description");
+const planetText = document.querySelector(".planet-text");
+const planetLongDescription = document.querySelector(".planet-long-description");
+
+const contentWrapper = document.querySelector(".content-wrapper");
+const controlsWrapper = document.querySelector(".controls");
+const statsWrapper = document.querySelector(".stats-wrapper");
+
+const statsValues = document.querySelectorAll(".stat-value");
+const statUnit = document.querySelector(".stat-unit");
+
+
+const wrapper = document.querySelector(".wrapper");
+const controls = document.querySelectorAll(".control");
+const planets = document.querySelectorAll(".planet");
+
+let currentPlanet = 0;
+
+function setProperties(planet, i) {
+    planet.style.setProperty("--trans-y", transformData[i].transY + "%");
+    planet.style.setProperty("--trans-x", transformData[i].transX + "%");
+    planet.style.setProperty("--scale", transformData[i].scale);
+    planet.style.setProperty("--opacity", transformData[i].opacity);
+
+    if (i == 1 || i == 2 || i == 3 || i == 5 || i == 7)
+        planet.style.visibility = "visible";
+    else planet.style.visibility = "hidden";
+}
+
+function updateStatistics(i) {
+    statsValues[0].innerText = data[i].lightTime;
+    statsValues[1].innerText = data[i].namesake;
+    statsValues[2].innerText = data[i].sunDistance[0];
+    statsValues[3].innerText = data[i].type;
+    statsValues[4].innerText = data[i].yearLength[0];
+    statsValues[5].innerText = data[i].moons;
+    statUnit.innerText = data[i].yearLength[1];
+}
+
+function selectPlanet(index) {
+    if (index > planets.length - 1 || index < 0) return;
+    setProperties(planets[index], 1);
+
+    if (index < planets.length - 1) {
+        setProperties(planets[index + 1], 2);
+        if (index < planets.length - 2)
+            setProperties(planets[index + 2], 3);
+    }
+
+    for (let i = 0; i < index; i++) {
+        setProperties(planets[i], 0);
+    }
+
+    for (let i = index + 3; i < planets.length; i++) {
+        setProperties(planets[i], 4)
+    }
+
+    controls[currentPlanet].classList.remove("control__select");
+    controls[index].classList.add("control__select");
+
+    wrapper.style.setProperty("--color", data[index].color);
+
+
+    let planetNameLetters = planetName.childNodes;
+    let planetNameLength = planetName.innerText.length;
+
+    for (let i = 0; i < planetNameLength; i++) {
+        planetNameLetters[i].classList.add("hide");
+        planetNameLetters[i].style.transitionDelay = i * 20 + "ms";
+    }
+
+    planetNamber.classList.add("hide");
+
+    setTimeout(() => {
+        let text = "";
+        for (let i = 0 ; i < data[index].name.length; i++) {
+            text += `<span class="hide" style="transition-delay: ${i * 20}ms">${data[index].name[i]}</span>`
+        }
+
+        planetName.innerHTML = text;
+
+        planetNamber.innerText = index + 1;
+
+
+        setTimeout(() => {
+            planetNameLetters = planetName.childNodes;
+            planetNameLength = planetName.innerText.length;
+
+            for (let i = 0; i < planetNameLength; i++) 
+                planetNameLetters[i].classList.remove("hide");
+            planetNamber.classList.remove("hide");
+        }, 100);
+
+
+    }, planetName.childNodes.length * 20);
+
+
+    planetDescriptions[currentPlanet].classList.add("hide");
+    planetDescriptions[index].classList.remove("hide");
+
+    planetLongDescription.innerText = data[index].longDesc;
+    updateStatistics(index);
+
+    
+    currentPlanet = index;
+}
+
+
+
+for (let i = 0; i < planets.length; i++) {
+    planets[planets.length - i - 1].style.zIndex = i;
+    planets[i].style.setProperty("--color", data[i].color);
+
+    controls[i].childNodes[0].innerText =  data[i].name
+    controls[i].addEventListener("click", ()=> {
+        selectPlanet(i);
+    })
+
+    planetDescriptions[i].innerText = data[i].shortDesc;
+    planetDescriptions[i].classList.add("hide");
+}
+
+
+
+
+
+let isCapturingScroll = false; 
+
+infoBtn.addEventListener("click", ()=> {
+    isCapturingScroll = true;
+    wrapper.style.cursor = "auto";
+    setProperties(planets[currentPlanet], 5);
+
+    if (currentPlanet < planets.length - 1) {
+        setProperties(planets[currentPlanet + 1], 6);
+        if (currentPlanet < planets.length - 2)
+            setProperties(planets[currentPlanet + 2], 6);
+    }
+
+
+    infoBtn.classList.add("scroll__btn");
+
+    planetNamber.classList.add("hide");
+
+    controls.forEach((control, i) => {
+        control.style.setProperty("--trans-delay", i*50+"ms")
+        control.classList.add("hide");
+    });
+
+    controlsWrapper.style.setProperty("--trans-delay", controls.length * 60 + "ms");
+    controlsWrapper.classList.add("hide");
+
+    planetDescriptions[currentPlanet].classList.add("hide");
+
+
+    contentWrapper.classList.add("more-info");
+    contentWrapper.scrollTo(0,0);
+
+
+
+    // scroll listener
+    let isScrolled = false;
+    statsWrapper.classList.add("hide");
+    contentWrapper.addEventListener("scroll", () => {
+
+        if (!isCapturingScroll) return;
+        
+        if (contentWrapper.scrollTop  > planetText.clientHeight / 2) {
+            if (isScrolled) return;
+            isScrolled = true;
+            contentWrapper.scrollTo(0,planetText.scrollHeight);
+            setProperties(planets[currentPlanet], 7);
+            planetText.classList.add("hide");
+            statsWrapper.classList.remove("hide");
+
+        } else {
+            if (!isScrolled) return;
+            isScrolled = false;
+            contentWrapper.scrollTo(0,0);
+            setProperties(planets[currentPlanet], 5);
+            planetText.classList.remove("hide");
+            statsWrapper.classList.add("hide");
+        }
+    })
+});
+
+closeInfoBtn.addEventListener("click" , () => {
+    isCapturingScroll = false;
+    selectPlanet(currentPlanet);
+    planetText.classList.remove("hide");
+    infoBtn.classList.remove("scroll__btn");
+    planetNamber.classList.remove("hide");
+
+
+    controls.forEach((control, i) => {
+        control.classList.remove("hide");
+    });
+    controlsWrapper.style.setProperty("--trans-delay", "0ms");
+    controlsWrapper.classList.remove("hide");
+
+
+    contentWrapper.classList.remove("more-info");
+});
+
+
+
+let touchstart = 0;
+let touchend = 0;
+
+function handleTouchs(direction) {
+    if (direction) selectPlanet(currentPlanet + 1);
+    else selectPlanet(currentPlanet - 1);
+}
+
+wrapper.addEventListener("touchstart", (e) => {
+    touchstart = e.changedTouches[0].screenY;
+});
+
+wrapper.addEventListener("touchend", (e) => {
+    touchend =  e.changedTouches[0].screenY;
+
+    if (touchend - touchstart < 100 && touchend - touchstart > -100) return;
+
+    if (e.target == infoBtn) return;
+    if (isCapturingScroll) return;
+    handleTouchs(touchend > touchstart);
+});
+
+
+//  mouse drag
+let mousestart = 0;
+let mouseend = 0;
+wrapper.addEventListener("mousedown", (e) => {
+    mousestart = e.screenY;
+    wrapper.style.cursor = "grabbing";
+});
+
+wrapper.addEventListener("mouseup", (e) => {
+    mouseend = e.screenY;
+    wrapper.style.cursor = "grab";
+
+    if (mouseend - mousestart < 100 && mouseend - mousestart > -100) return;
+
+    if (e.target == infoBtn) return;
+    if (isCapturingScroll) return;
+    handleTouchs(mouseend > mousestart);
+});
+
+
+
+selectPlanet(currentPlanet);
